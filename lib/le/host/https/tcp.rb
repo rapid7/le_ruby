592ee929d078738f0921cd4ee961da7e59340fe5
Mark Lacomber <marklacomber@gmail.com>
2011-08-08 01:53:38 +0200

    Fixed Connection Timeout Bug, Added auto-reconnect, Gem upgraded to Version 1.5

--- lib/le/host/https/tcp.rb
+++ lib/le/host/https/tcp.rb
@@ -13,33 +13,49 @@ module Le
 
       class TCPSOCKET
 
-	attr_accessor :sock, :conn
+	attr_accessor :sock, :conn, :key, :location
 	def initialize(key, location)
 
-          # Create the unique address comprising of user-key and location of file on logentries server
-	  addr = sprintf('/%s/hosts/%s/?realtime=1', key, location)	  
-	
-          # Open the TCP connection to the Logentries Server
-	  @sock = TCPSocket.new('api.logentries.com', 443)
+          @key = key
+          @location = location
+	  begin
+          	createSocket(key, location)
+	  rescue OpenSSL::SSL::SSLError, TimeoutError, Errno::EHOSTUNREACH, Errno::ECONNREFUSED, Errno::ECONNRESET, Errno::ETIMEDOUT, EOFError => e
+		$stderr.puts "WARNING: #{e.class} creating the connection to Logentries. #{e.message}"
+          end
+	end
 
-	  @conn = OpenSSL::SSL::SSLSocket.new(@sock, OpenSSL::SSL::SSLContext.new())
-	  @conn.sync_close = true
+        def createSocket(key, location)
+	  
+          addr = sprintf('/%s/hosts/%s/?realtime=1', key, location)
+
+          # Open the TCP connection to the Logentries Server
+          @sock = TCPSocket.new('api.logentries.com', 443)
+
+          @conn = OpenSSL::SSL::SSLSocket.new(@sock, OpenSSL::SSL::SSLContext.new())
+          @conn.sync_close = true
           @conn.connect
 
           # Set up connection with Logentries API to receive messages in chunks, i.e, logs
-	  request = sprintf("PUT %s HTTP/1.1\r\n", addr)
+          request = sprintf("PUT %s HTTP/1.1\r\n", addr)
           @conn.print(request)
-	  @conn.print("Accept-Encoding: identity\r\n")
-	  @conn.print("Transfer_Encoding: chunked\r\n\r\n")
-	end
+          @conn.print("Accept-Encoding: identity\r\n")
+          @conn.print("Transfer_Encoding: chunked\r\n\r\n")
+
+        end
 	
 	def deliver(message)
 
           # Sends the log to the Logentries Server
           begin
 	    @conn.print(message + "\r\n")
-	  rescue OpenSSL::SSL::SSLError, TimeoutError, Errno::ECONNRESET, EOFError => e
-	    $stderr.puts "WARNING: #{e.class} sending log #{message}"
+	  rescue OpenSSL::SSL::SSLError, TimeoutError, Errno::EHOSTUNREACH, Errno::ECONNREFUSED, Errno::ENOTCONN, Errno::ECONNRESET, Errno::ETIMEDOUT, EOFError => e
+	    $stderr.puts "WARNING: #{e.class} sending log #{e.message}"
+              begin
+                createSocket(@key, @location)
+              rescue OpenSSL::SSL::SSLError, TimeoutError, Errno::EHOSTUNREACH, Errno::ECONNREFUSED, Errno::ECONNRESET, Errno::ETIMEDOUT, EOFError => e
+                $stderr.puts "WARNING: #{e.class} creating the connection to Logentries. #{e.message}"
+              end
 	  end
         end
 	
