e3bcb1bfac1ce2c1a80eb3391c17dee8abbd019a
Thomas Gautier <thomas.gautier@gmail.com>
2012-12-31 15:33:15 +0100

    Cleaning identation and tabs

--- lib/le/host.rb
+++ lib/le/host.rb
@@ -2,31 +2,29 @@ module Le
   module Host
 
     def self.new(token, local)
-
       Le::Host::HTTP.new(token, local)
-      
     end
-  
+
     module InstanceMethods
       def formatter
-	proc do |severity, datetime, progname, msg|
-	  	message = "#{datetime} "
-      	message << format_message(msg, severity)
-    end
-	end
-  
-      def format_message(message_in, severity)
-		message_in = message_in.lstrip
+        proc do |severity, datetime, progname, msg|
+          message = "#{datetime} "
+          message << format_message(msg, severity)
+        end
+      end
 
-		message_out = ""
-		message_out = "severity=#{severity}, "
-		case message_in
-		when String
-		  message_out << message_in
-		else
-		  message_out << message_in.inspect
-		end
-		message_out
+      def format_message(message_in, severity)
+        message_in = message_in.lstrip
+
+        message_out = ""
+        message_out = "severity=#{severity}, "
+        case message_in
+        when String
+          message_out << message_in
+        else
+          message_out << message_in.inspect
+        end
+        message_out
       end
     end
   end
